5d05d040fead4f8d0563345555fc9fe292a3def9
oem <darkoem@gmail.com>
2010-12-07 17:25:21 +0100

    Import new empty rails project: Develop a simple blog BDD-style

---
+++ level_02/blog/README.md
@@ -0,0 +1,8 @@
+# Create a blog using BDD
+
+The classic Rails app done BDD-style!
+
+## Steps
+
+We don't want the Test::Unit stuff (-T)  
+`rails new blog -T`
---
+++ level_02/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'c07ef3d62cf122b9dac3f827dbba5911b7499dbbe2c924ee336d93a7a46d405c69f5de8b02e8bc94458811fc205f3be3fd71be43ca2f9407b16b472d7f07cf06'
