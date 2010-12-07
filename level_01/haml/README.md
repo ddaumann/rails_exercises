2dfa58c047b34e6f71662b838b3dfb0eb8428948
oem <darkoem@gmail.com>
2010-12-07 16:27:06 +0100

    Create rails app to demonstrate usage of haml

---
+++ level_01/haml/README.md
@@ -0,0 +1,12 @@
+# Using haml with rails
+
+Using haml instead of erb is very simple:
+
+Gemfile:
+
+`gem "haml"`
+
+Now haml will render all view templates that end with .haml.  
+You can start by simply renaming all your erbs to hamls, haml acts as drop in for erb.
+
+When you are ready you can start to convert your erb files or completely rewrite them.
---
+++ level_01/haml/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Haml::Application.config.secret_token = 'abbb31719a4485fbe543f43ddcb9547fcac3f5ee7eb3d3800830b81fe1ae18e048dd8e96593b89884e21342327cb7b28d0b50db863121160b73aa5c628f1d1ea'
