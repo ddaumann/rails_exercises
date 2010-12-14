8e709d18f8989e32a9eb9ceeaccef7e2874ce1bc
oem <darkoem@gmail.com>
2010-12-14 14:55:32 +0100

    Drive out edit template

--- level_02/blog/spec/views/articles/edit.html.haml_spec.rb
+++ level_02/blog/spec/views/articles/edit.html.haml_spec.rb
@@ -1,4 +1,26 @@
 require "spec_helper"
 
 describe "articles/edit.html.haml" do
+  before { @article = mock_model(Article, :title => "Foo").as_null_object }
+
+  it "renders a form for @article" do
+    render
+    rendered.should have_selector("form", :action => article_path(@article), :method => "post") do |form|
+      form.should have_selector("input", :type => "submit", :value => "Submit")
+    end
+  end
+
+  it "renders an input field for the title" do
+    render
+    rendered.should have_selector("form") do |form|
+      form.should have_selector("input", :type => "text", :name => "article[title]")
+    end
+  end
+  
+  it "renders a text area for the content" do
+    render
+    rendered.should have_selector("form") do |form|
+      form.should have_selector("textarea", :name => "article[content]")
+    end
+  end
 end
